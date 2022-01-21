84f94d34c391fb6d563d2dce987f02d066d66985
SaadMOUSTAKIM <saadmoustakim2@gmail.com>
2022-01-21 12:13:51 +0100

    Debut acces au base de donnes

--- routes/movies.js
+++ routes/movies.js
@@ -1,13 +1,12 @@
 var express = require("express");
-
 var router = express.Router();
+const fs = require("fs");
 
 /* GET home page. */
-
 router.get("/", function (req, res, next) {
-
-res.send("respond from movies");
-
+  let movies = fs.readFileSync("./movies_reduit.json");
+  let moviesjs = JSON.parse(movies);
+  res.send(moviesjs);
 });
 
 module.exports = router;
